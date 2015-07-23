081c8ccaa52328be2d4e836cac729394a70a5834
Russell Hueske <hueske.russ690@gmail.com>
2015-07-23 14:48:45 -0500

    ugh

--- obd_arduino.ino
+++ obd_arduino.ino
@@ -24,25 +24,25 @@ void sendJSON() {
     root["rpm"] = value;
   }
   if (obd.read(PID_ENGINE_LOAD, value)) {
-    root['engine_load'] = value;
+    root["engine_load"] = value;
   }
   if (obd.read(PID_COOLANT_TEMP, value)) {
-    root['coolant_temp'] = value;
+    root["coolant_temp"] = value;
   }
   if (obd.read(PID_ABSOLUTE_ENGINE_LOAD, value)) {
-    root['abs_engine_load'] = value;
+    root["abs_engine_load"] = value;
   }
   if (obd.read(PID_TIMING_ADVANCE, value)) {
-    root['timing_advance'] = value;
+    root["timing_advance"] = value;
   }
   if (obd.read(PID_ENGINE_OIL_TEMP, value)) {
-    root['engine_oil_temp'] = value;
+    root["engine_oil_temp"] = value;
   }
   if (obd.read(PID_ENGINE_TORQUE_PERCENTAGE, value)) {
-    root['engine_torque_percentage'] = value;
+    root["engine_torque_percentage"] = value;
   }
   if (obd.read(PID_ENGINE_REF_TORQUE, value)) {
-    root['engine_ref_torque'] = value;
+    root["engine_ref_torque"] = value;
   }
 //  read from OBD into root
 //  print back to wire.
