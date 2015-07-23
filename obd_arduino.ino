a111554739e1eb21f5488431045fe5dda37b0ca8
Russell Hueske <hueske.russ690@gmail.com>
2015-07-23 14:43:57 -0500

    ugh

--- obd_arduino.ino
+++ obd_arduino.ino
@@ -28,5 +28,30 @@ void sendJSON() {
 }
 
 void readOBD(JsonObject &root) {
-  
+  // Engine
+  int value;
+  if (obd.read(PID_RPM, value)) {
+    root['rpm'] = value;
+  }
+  if (obd.read(PID_ENGINE_LOAD, value)) {
+    root['engine_load'] = value;
+  }
+  if (obd.read(PID_COOLANT_TEMP, value)) {
+    root['coolant_temp'] = value;
+  }
+  if (obd.read(PID_ABSOLUTE_ENGINE_LOAD, value)) {
+    root['abs_engine_load'] = value;
+  }
+  if (obd.read(PID_TIMING_ADVANCE, value)) {
+    root['timing_advance'] = value;
+  }
+  if (obd.read(PID_ENGINE_OIL_TEMP, value)) {
+    root['engine_oil_temp'] = value;
+  }
+  if (obd.read(PID_ENGINE_TORQUE_PERCENTAGE, value)) {
+    root['engine_torque_percentage'] = value;
+  }
+  if (obd.read(PID_ENGINE_REF_TORQUE, value)) {
+    root['engine_ref_torque']
+  }
 }
